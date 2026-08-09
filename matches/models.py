e6a56bba31a65d275f386977793c37fabb355aca
Your name <you@example.com>
2026-08-09 16:53:15 +0300

    Спорт сайт

---
+++ matches/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    location = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    team1 = models.CharField(max_length=100)
+    team2 = models.CharField(max_length=100)
+    score_team1 = models.IntegerField(default=0)
+    score_team2 = models.IntegerField(default=0)
+    winner = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.team1} - {self.team2}"
